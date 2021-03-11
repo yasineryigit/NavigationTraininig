55d8a798c74fff3fcd209e3b315d8e4b76d4ee25
sancaryum <yasineryigit737@gmail.com>
2021-03-11 05:06:54 +0300

    First direction added

---
+++ app/src/main/java/com/ossovita/navigationtraining/FirstFragment.java
@@ -0,0 +1,38 @@
+package com.ossovita.navigationtraining;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment {
+    Button button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        button = view.findViewById(R.id.first_fragment_button);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavDirections directions = FirstFragmentDirections .actionFirstFragmentToSecondFragment();
+                Navigation.findNavController(view).navigate(directions);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ossovita/navigationtraining/SecondFragment.java
@@ -0,0 +1,30 @@
+package com.ossovita.navigationtraining;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SecondFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
